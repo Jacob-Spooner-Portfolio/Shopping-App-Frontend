2270c5f28ec1349c57c8d951a3758ec78c103a57
lleviathn <jacobspooner92@gmail.com>
2024-03-30 14:19:54 -0500

    Updated base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axios-config'
+import { useState, useEffect } from 'react';
+import Layout from './components/Layout'
+import Home from "./components/home/Home"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+
+  const [products, setProducts] = useState();
+
+  const getProducts = async () =>{
+
+    try {
+
+      const response = await api.get("/api/v1/products")
+
+      setProducts(response.data)
+
+    } catch (err) {console.log(err)}
+  }
+
+  useEffect(() => {
+    getProducts();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}>
+
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
