d5866e49c13569d0e3803d6285eb010389350995
lleviathn <jacobspooner92@gmail.com>
2024-03-30 20:55:03 -0500

    setup product view limiting

--- src/components/product_list/Product_List.js
+++ src/components/product_list/Product_List.js
@@ -3,17 +3,30 @@ import '.././product_card/Product_Card'
 import React from 'react'
 import Product_Card from '.././product_card/Product_Card'
 
+let minIndex = 0;
+let pageNum = 1;
+let productsOnPage = 10;
+
 const Product_List = ({products}) => {
   return (
     <div className='product-card-list-container'>
+        <div className='product-card-list'>
+            {
+                products.map((product, productIndex, productArr) =>{
+                    return(
+                        <div className='product-card'>
+                            <Product_Card product = {product} />
+                        </div>
+                    )
+                })
+            }
+        </div>
         {
-            products.map((product) =>{
-                return(
-                    <div>
-                        <Product_Card product = {product} />
-                    </div>
-                )
-            })
+            <ul className='products-per-page-container'>
+                <input onClick={productsOnPage=10} type='button' id='products-10'></input>
+                <input onClick={productsOnPage=25} type='button' id='products-25'></input>
+                <input onClick={productsOnPage=50} type='button' id='products-50'></input>
+            </ul>
         }
     </div>
   )
