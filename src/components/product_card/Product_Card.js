4b11667377215f78bb987e89313c0b159b585c46
lleviathn <jacobspooner92@gmail.com>
2024-03-31 12:49:47 -0500

    Fixed css styling to make things more uniform

--- src/components/product_card/Product_Card.js
+++ src/components/product_card/Product_Card.js
@@ -3,10 +3,12 @@ import './Product_Card.css'
 const Product_Card = ({product}) => {
   return (
     <div className='product-card'>
-        <img src={product.main_image} alt=''></img>
-        <h3>{product.title}</h3>
-        <p>${parseFloat(product.price)}</p>
-        <op>{product.availability}</op>
+        <img src={product.main_image} alt={product.title} className='product-image'/>
+        <div className='product-info'>
+            <h3>{product.title}</h3>
+            <p>${parseFloat(product.price)}</p>
+            <op>{product.availability}</op>
+        </div>
     </div>
   )
 }
